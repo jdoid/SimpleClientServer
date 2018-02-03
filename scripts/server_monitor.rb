8fb93aad51fb6811f87bc8ce5276f27fa35adb9d
Dean Kirby <8877692+jdoid@users.noreply.github.com>
2018-02-03 15:17:37 -0700

    add initial code

--- scripts/server_monitor.rb
+++ scripts/server_monitor.rb
@@ -1,1 +1,25 @@
+require 'socket'
 
+server = TCPServer.open(16201)
+
+cli = lambda { |command| command }
+
+loop {
+  client = server.accept
+  client.puts(Time.now.ctime)
+  client.puts
+
+   # users logged in
+  client.puts(cli.call(`users`))
+  client.puts
+
+  # Minecraft server status
+  client.puts(cli.call(`msm server list`))
+  client.puts
+
+  # Plex server running
+  client.puts(cli.call(`ps -ef | grep "Plex Media Server" | grep -v grep`))
+  client.puts
+
+  client.close
+}
