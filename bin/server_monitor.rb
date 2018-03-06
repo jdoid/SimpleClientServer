078a61ee37bf019e203ae7aef87657039dcd4087
Dean Kirby <8877692+jdoid@users.noreply.github.com>
2018-03-06 20:41:33 +0000

    add function write_to_file and change function execute to send_status. steps toward email status

--- bin/server_monitor.rb
+++ bin/server_monitor.rb
@@ -5,10 +5,18 @@ server = TCPServer.open(port)
 
 $cli = lambda { |command| command }
 
-def execute
-  File.foreach('sys_commands').with_index do |line|
-      $client.puts($cli.call(`#{line}`))
-      $client.puts
+def write_to_file
+  open('status.out', 'a') do |f|
+    File.foreach('sys_commands').with_index do |line|
+      f.puts ($cli.call(`#{line}`))
+      f.puts
+    end
+  end
+end
+
+def send_status
+  File.foreach('status.out').with_index do |line|
+      $client.puts(line)
     end
 end
 
@@ -18,7 +26,10 @@ loop {
   $client.puts(Time.now.ctime)
   $client.puts
   
-  execute
+  File.delete "status.out"
+  write_to_file
+  send_status
+  
 
   $client.close
 }
